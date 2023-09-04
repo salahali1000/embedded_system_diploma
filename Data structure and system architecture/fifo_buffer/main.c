b61fd92f13a0e3298bfe48bdd625ac963ac7150b
salahali1000 <124151783+salahali1000@users.noreply.github.com>
2023-09-05 01:27:10 +0300

    Create main.c

---
+++ Data structure and system architecture/fifo_buffer/main.c
@@ -0,0 +1,34 @@
+/*
+  main.c
+ *
+ *  Created on: Sep 4, 2023
+ *      Author: salah
+ */
+#include "fifo.h"
+int main()
+{
+	element_type i,temp;
+	FIFO_Buf_t fifo_uart;
+	if(FIFO_init( &fifo_uart , uart_buffer , 5 )==FIFO_no_error)
+		printf("fifo init ----------- Done\n");
+
+	for(i=0 ; i<7 ; i++)
+	{
+		printf("fifo enqueue %x\n",i);
+		if(FIFO_enqueue( &fifo_uart , i )==FIFO_no_error)
+			printf("\t fifo enqueue ----------- Done\n");
+		else
+			printf("\t fifo enqueue ----------- failed\n");
+
+	}
+	FIFO_Print( &fifo_uart );
+	if(FIFO_dequeue( &fifo_uart , &temp )==FIFO_no_error)
+				printf("\t fifo dequeue ----------- Done\n");
+	if(FIFO_dequeue( &fifo_uart , &temp )==FIFO_no_error)
+				printf("\t fifo dequeue ----------- Done\n");
+	FIFO_Print( &fifo_uart );
+
+	return 0;
+}
+
+
